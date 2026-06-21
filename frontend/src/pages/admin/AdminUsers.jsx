e5411efc7f9e0b958a42e4f2f6f49ed7ec2cad46
sshkhndmd <hosslerjx@gmail.com>
2026-06-21 16:44:42 +0500

    Fix admin

--- frontend/src/pages/admin/AdminUsers.jsx
+++ frontend/src/pages/admin/AdminUsers.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { api } from "../../api";
 
-const ROLES = ["CUSTOMER", "ADMIN", "PRODUCT_MANAGER", "SALES_MANAGER", "RETURNS_MANAGER"];
+const ROLES = ["CUSTOMER", "ADMIN", "PRODUCT_MANAGER", "SALES_MANAGER"];
 
 export default function AdminUsers() {
   const token = localStorage.getItem("token");
